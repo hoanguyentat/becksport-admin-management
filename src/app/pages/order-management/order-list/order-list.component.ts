1839c4286be3d1f9991a94cea6c3a73d58795bba
iot-chiennm <iot-chiennm@ViettelGroup.ViettelAD.com>
2018-06-27 14:43:42 +0700

    add demo order list

--- src/app/pages/order-management/order-list/order-list.component.ts
+++ src/app/pages/order-management/order-list/order-list.component.ts
@@ -1,4 +1,87 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+
+const MOCK_DATA = {
+  "paging": {
+    "page": 1,
+    "total": 200,
+    "page_size": 2
+  },
+  "data": [
+    {
+      "id": "653678611405325",
+      "user_backend_id": "653678611405322",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    },
+    {
+      "id": "653678611405321",
+      "user_backend_id": "653678611405324",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    },
+    {
+      "id": "653678611405321",
+      "user_backend_id": "653678611405324",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    },
+    {
+      "id": "653678611405321",
+      "user_backend_id": "653678611405324",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    },
+    {
+      "id": "653678611405321",
+      "user_backend_id": "653678611405324",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    },
+    {
+      "id": "653678611405321",
+      "user_backend_id": "653678611405324",
+      "customer_name": "Phuong TH",
+      "customer_address": "Phương Mai, Hà Nội",
+      "total_products": 2,
+      "total_price": 1000000,
+      "price_unit": "VND",
+      "updated_at": 1529912852,
+      "created_at": 1529912852,
+      "user_backend_name": "Quang LH"
+    }
+  ]
+};
 
 @Component({
   selector: 'app-order-list',
@@ -7,9 +90,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
+  settings = {
+    hideSubHeader: true,
+    mode: 'external',
+    edit: {
+      editButtonContent: '<i class="fa fa-edit"></i>',
+      saveButtonContent: '<i class="fa fa-checkmark"></i>',
+      cancelButtonContent: '<i class="fa fa-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="fa fa-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      customer_name: {
+        title: 'Tên khách hàng',
+        type: 'string'
+      },
+      customer_address: {
+        title: 'Địa chỉ',
+        type: 'string'
+      },
+      total_products: {
+        title: 'Số sản phẩm',
+        type: 'number'
+      },
+      total_price: {
+        title: 'Tổng giá',
+        type: 'number'
+      },
+      price_unit: {
+        title: 'Mệnh giá',
+        type: 'string'
+      },
+      updated_at: {
+        title: 'Cập nhập',
+        type: 'number'
+      },
+      created_at: {
+        title: 'Tạo',
+        type: 'number'
+      },
+    }
+  }
+
+  source: LocalDataSource = new LocalDataSource();
+
   constructor() { }
 
   ngOnInit() {
+    this.source.load(MOCK_DATA.data)
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
   }
 
 }
--- src/app/pages/order-management/order-management.module.ts
+++ src/app/pages/order-management/order-management.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { OrderListComponent } from './order-list/order-list.component';
 import { SharedModule } from '../../shared/shared.module';
 import { OrderManagemenRoutingtModule } from './order-management-routing.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
   imports: [
     CommonModule,
     OrderManagemenRoutingtModule,
-    SharedModule
+    SharedModule,
+    Ng2SmartTableModule
   ],
   declarations: [OrderListComponent]
 })
