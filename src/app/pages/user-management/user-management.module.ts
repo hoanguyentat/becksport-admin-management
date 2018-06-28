89ac454af9f7847f2f256a6676f5d7af004e1c09
phuongdv <dovietphuong.ptit@gmail.com>
2018-06-28 09:52:41 +0700

    test user function

--- src/app/pages/user-management/user-management.module.ts
+++ src/app/pages/user-management/user-management.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NbLayoutModule,
+    NbSidebarModule
   ],
-  declarations: []
+  declarations: [NbSidebarModule]
 })
 export class UserManagementModule { }
