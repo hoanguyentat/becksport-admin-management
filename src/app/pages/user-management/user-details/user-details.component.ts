35399050a1dce5d68fc9a39f6946e42a090d4cd4
phuongdv <dovietphuong.ptit@gmail.com>
2018-06-28 18:52:18 +0700

    add user management

--- src/app/pages/user-management/user-details/user-details.component.ts
+++ src/app/pages/user-management/user-details/user-details.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
+var MOCK_DATA = {
+    "id": "653678611405322",
+    "username": "Phuong NM",
+    "fullname": "Phuong Nguyen Minh",
+    "sex": "male",
+    "email": "phuongnm@bkav.com",
+    "phone": "0191512355",
+    "birth_day": "2018-06-01",
+    "role": "ADMIN",
+    "created_at": 1529913941,
+    "updated_at": 1529913941
+}
+
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
--- src/app/pages/user-management/user-list/user-list.component.ts
+++ src/app/pages/user-management/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { Router } from '@angular/router';
 
 
 const MOCK_DATA = {
@@ -90,9 +92,68 @@ const MOCK_DATA = {
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+settings = {
+    hideSubHeader: true,
+    mode: 'external',
+    edit: {
+      editButtonContent: '<i class="fa fa-edit"></i>',
+      saveButtonContent: '<i class="fa fa-checkmark"></i>',
+      cancelButtonContent: '<i class="fa fa-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="fa fa-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      customer_name: {
+        title: 'Tên khách hàng',
+        type: 'string'
+      },
+      customer_address: {
+        title: 'Địa chỉ',
+        type: 'string'
+      },
+      total_products: {
+        title: 'Số sản phẩm',
+        type: 'number'
+      },
+      total_price: {
+        title: 'Tổng giá',
+        type: 'number'
+      },
+      price_unit: {
+        title: 'Mệnh giá',
+        type: 'string'
+      },
+      updated_at: {
+        title: 'Cập nhập',
+        type: 'number'
+      },
+      created_at: {
+        title: 'Tạo',
+        type: 'number'
+      },
+    }
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  	this.source.load(MOCK_DATA.data);
+  }
+
+   onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  onView(event): void {
+    this.router.navigate(['pages/order-management/order-detail/', event.data.id]);
   }
 
 }
